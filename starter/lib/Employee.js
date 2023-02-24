ecaa644f249cf192594f2e10a65215899f76c060
Yusen22 <elijahwilsher@gmail.com>
2023-02-24 18:03:43 +0000

    added Employee class with functions

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+
+const employee = class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getID() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return this.constuctor.name;
+    }
+
+}
+
+module.exports = employee
