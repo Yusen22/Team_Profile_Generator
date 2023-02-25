2954e6dbb3c1358498ad0455961459b9f1a5c0be
Yusen22 <elijahwilsher@gmail.com>
2023-02-25 11:07:28 +0000

    added Engineer subclass

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+ 
+    constructor(name, id, email, github) {
+        super(name, id, email)
+        this.github = `https://github.com/${github}`
+    }
+    getRole() {
+        console.log(this.constructor.name);
+    }
+    getGithub() {
+        console.log(this.github)
+    }
+   
+}
+
+const eng1 = new Engineer("Kev", 1, 2, 'bigKev')
+
+eng1.getRole()
+
+module.exports = Engineer
